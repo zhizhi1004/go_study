76d1070706d0a1ec40a2c6eeb2f26c53a4cf9bf6
hao-yutong <645160224@qq.com>
2025-10-19 00:36:07 +0800

    合并区间

---
+++ task01/merge.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func twoSum(nums []int, target int) []int {
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i]+nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
+	return []int{}
+}
+
+func main() {
+	nums := []int{2, 7, 11, 15}
+	target := 9
+	result := twoSum(nums, target)
+	fmt.Println(result)
+}
